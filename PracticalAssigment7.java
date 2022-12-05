c2e9b2085307a63d07682499debcf6fdc544f584
smit <monparasmit1@gmail.com>
2022-12-05 19:14:30 +0530

    add PA7

--- PracticalAssigment7.java
+++ PracticalAssigment7.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class PracticalAssigment7 {
     public static void main(String[] args) {
-        
+        LocalDateTime date = LocalDateTime.now();
+        System.out.println(date);
     }
 }
